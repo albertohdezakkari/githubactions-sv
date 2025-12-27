16446779924ad16ab05d4e14151874a8eb59a60f
Alberto Hernandez <akkarihdez@gmail.com>
2025-12-27 17:29:39 +0100

    test 01

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ export class AppController {
   @Get()
   getHello(): string {
     // const saludo = 'Hola NestJS'; // ❌ variable no usada
-    const saludo = 'Hola NestJS';
+    const saludo = 'Hola mundo';
 
     // return 'Hola mundo';
     return saludo;
